ae0525eb7dfb01a441b3b8101418354a29b7d72c
SkyPH03 <81352625+SoraShiro03@users.noreply.github.com>
2022-03-22 20:52:34 +0630

    starter setup preparing!

---
+++ src/components/userAuthForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function UserAuthForm() {
+    return (
+        <div></div>
+    )
+}
+
+export function Register() {
+    return (
+        <div></div>
+    )
+}
+
+export function Login() {
+    return (
+        <div></div>
+    )
+}
+
+export function Logout() {
+    return (
+        <div></div>
+    )
+}
